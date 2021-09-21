84845b44593d5db3bb81e4b85231e44e118e8506
Тамара Мартышева <Tomoe.yokay730@yandex.ru>
2021-09-21 16:33:33 +0400

    world added

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleAppHello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
     }
